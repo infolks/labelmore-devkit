caa648444962bd7f9636d5c8660dd7b2d3badd7d
arjunatlast <arjunatlast@gmail.com>
2019-06-11 15:28:19 +0530

    Patch changes

--- dist/modules/extras/labels/defaults.d.ts
+++ dist/modules/extras/labels/defaults.d.ts
@@ -18,6 +18,6 @@ export interface ContourLabel extends Label {
         points: {
             x: number;
             y: number;
-        };
+        }[];
     };
 }
--- src/modules/extras/labels/defaults.ts
+++ src/modules/extras/labels/defaults.ts
@@ -20,6 +20,6 @@ export interface ContourLabel extends Label{
         points: {
             x: number,
             y: number
-        }
+        }[]
     }
 }
